e5dcc857e6fd674aacd06815e847cc753909734e
bilalsuleman691-eng <bilalsuleman691@gmail.com>
2026-05-14 17:31:49 +0500

    Update print statement from 'Hello' to 'Goodbye'

--- script.js
+++ script.js
@@ -12,9 +12,6 @@ function saveAndRefresh() {
     renderAll();
 }
 
-const supabaseUrl = 'https://zeadgtkzqooiswyyuozl.supabase.co';
-const supabaseKey = 'sb_publishable_b4jLu7Bx2dsGtLR72i8dMA_0eGc0...'; // Yahan apni poori key paste karein
-const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
 /// --- UPDATED LOGIN SYSTEM ---
 function login() {
